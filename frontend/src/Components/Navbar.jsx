566f08a43b27f4abad4102410fa72a5dccf4af55
Deep <kapateldeep49@gmail.com>
2025-04-17 12:09:47 -0500

    Add some file

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <h1>To Do List</h1>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
