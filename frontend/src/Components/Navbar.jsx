48eca280d24df3b6e95b37051ee2300f88e24df7
Deep <kapateldeep49@gmail.com>
2025-04-28 13:24:51 -0500

    edit navbar with search functionality UI

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -5,12 +5,21 @@ import "./Navbar.css";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="navbar__logo">📝 To Do List</div>
-      <ul className="navbar__links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/tasks">Tasks</a></li>
-        <li><a href="/about">About</a></li>
-      </ul>
+      <div className="navbar__left">
+        <div className="navbar__logo">📝 To Do List</div>
+        <ul className="navbar__links">
+          <li><a href="/">Home</a></li>
+          <li><a href="/tasks">Tasks</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </div>
+      <form className="navbar__search" >
+        <input
+          type="text"
+          placeholder="Search tasks..."
+        />
+        <button type="submit">Search</button>
+      </form>
     </nav>
   );
 };
